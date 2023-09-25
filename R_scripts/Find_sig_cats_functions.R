ae9d700e7ae1a5dea53a407d537fc97363b54dae
Ion <83654855+ion1241@users.noreply.github.com>
2023-09-25 10:57:40 +0200

    Update and rename Find_sig_cats.R to Find_sig_cats_functions.R
    Add Kendall_function

--- R_scripts/Find_sig_cats_functions.R
+++ R_scripts/Find_sig_cats_functions.R
@@ -32,3 +32,19 @@ find_sig_cats=function(catTibble, metadata, groupVarName) {
   
   return(testDF)
 }
+
+
+Kendall_correlation <- function(dataframe, vector) {
+  results <- apply(dataframe, 1, function(row) {
+    cor_test <- cor.test(row, vector, method = "kendall")
+    rowname <- rownames(dataframe)[which(row == row)]
+    tau <- cor_test$estimate
+    p_value <- cor_test$p.value
+    c(row = row, tau = tau, p_value = p_value)
+  })
+  results <- as.data.frame(t(results))
+  colnames(results) <- c("ED10", "EN17", "EN20", "EOI15", "EOI20", "EOK10", "EU8", "LOK10", "Tau", "Sig")
+  return(results)
+}
+
+
